acb3c9682145c58d0a4b17f90f2960a0772e8228
marioguadalupe <mguadalupe@intellisys.com.do>
2019-07-12 08:09:54 -0400

    delete component adder and modified App to have a constructor method and pass it to Cart and Home

--- app/components/Adder.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var Total = require('./Total')
-
-function totalAdd(price){
-    var value = Total.totalPrice + price
-    return Total.totalPrice = value
-}
-
-module.exports = totalAdd;
--- app/components/App.js
+++ app/components/App.js
@@ -1,37 +1,56 @@
-var ArticlesList = require('../articles/articlesList.js');
-var Brands = require('./Brands.js');
-var Home = require('./Home.js');
-var Header = require('./Header.js');
-var Cart = require('./Cart.js');
-var React = require('react');
-var ReactDOM = require('react-dom');
-var ReactRouter = require('react-router-dom');
+var ArticlesList = require("../articles/articlesList.js");
+var Brands = require("./Brands.js");
+var Home = require("./Home.js");
+var Header = require("./Header.js");
+var Cart = require("./Cart.js");
+var React = require("react");
+var ReactRouter = require("react-router-dom");
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Switch = ReactRouter.Switch;
 
-
 class App extends React.Component {
-    render() {
-      return (
-        <Router>
-          <div>
-            <Header/>
-            
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/shop' component={Home} />
-              <Route exact path='/cart' component={Cart} />
+  constructor(props) {
+    super(props);
+    this.state = {
+      articlesInCart: []
+    }
+
+    this.addToCart = this.addToCart.bind(this);
+  }
+  
+  addToCart(article){
+    const articlesInCart = [...this.state.articlesInCart, article];
+    
+    this.setState({
+      articlesInCart
+    });
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Switch>
+              <Route exact path='/' render={() => {
+                return <Home addToCart={this.addToCart} articlesInCart={this.state.articlesInCart} />
+              }}/>
+              <Route exact path='/cart' render={() => {
+                return <Cart articlesInCart={this.state.articlesInCart} />
+              }}/>
               <Route render={function () {
                 return <p>Not Found</p>
                 }} />
-            </Switch>
-          </div>
-        </Router>
-        
-      )
-    }
-};
-
+            <Route
+              render={function() {
+                return <p>Not Found</p>;
+              }}
+            />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 module.exports = App;
