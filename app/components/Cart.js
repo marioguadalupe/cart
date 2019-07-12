53a58a2228b43aa10e42f989905b49e10038aa81
marioguadalupe <mguadalupe@intellisys.com.do>
2019-07-12 08:41:02 -0400

    Modify cart to receive the props from App, create a constructor method and a total and a componentDidMount methods

--- app/components/Cart.js
+++ app/components/Cart.js
@@ -1,15 +1,68 @@
 var React = require('react');
 var Total = require('./Total.js')
 
+
 class Cart extends React.Component {
-  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalPay:0
+    };
+    this.total = this.total.bind(this);
+  }
+
+  total(){
+    console.log(this.props);
+    const totalPay = this.props.articlesInCart.reduce(function(finalPrice, article){
+      return article.price + finalPrice
+    },this.state.totalPay);
+
+    this.setState({totalPay})
+  }
+
+  componentDidMount(){
+    this.total();
+  }
+
+
   render() {
     return (
       <div>
         <h1>
-            {Total.totalPrice}
+            Cart 
         </h1>
+        <ul>
+          {this.props.articlesInCart.map(function(article){
+            return (
+              <div>
+                <img src={article.image} alt={article.name}/>
+                <li key={article.brand}>{article.brand +' '+ article.name}</li>
+                <li key={article.status}>{`${article.status} $ ${article.price} `}</li>
+                
+              </div>
+            )
+          })}
+        </ul>
 
+        <ul>
+          <li>Coupon:</li>
+          <li>{`Subtotal: ${this.state.totalPay}`}</li>
+          <li>Shipping:</li>
+          <li>{`TOTAL: ${this.state.totalPay}`}</li>
+        </ul>
+        <button 
+          type="button" 
+          className="visa-checkout"
+          >
+            VISA Checkout 
+        </button>
+        <button 
+          type="button" 
+          className="checkout"
+          >
+            CHECKOUT 
+        </button>
       </div>   
     )
   }
